ebc3aa0e2286687f835fc2d849faf31c0264bf61
knnedy <kennymwendwadev@gmail.com>
2026-03-12 10:23:42 +0300

    feat: add user model and repository interface

---
+++ internal/domain/user.go
@@ -0,0 +1,20 @@
+package domain
+
+import "time"
+
+type User struct {
+	ID             string     `json:"id"`
+	Name           string     `json:"name"`
+	Email          string     `json:"email"`
+	HashedPassword string     `json:"password"`
+	CreatedAt      time.Time  `json:"createdAt"`
+	UpdatedAt      *time.Time `json:"updatedAt"`
+}
+
+type UserRepository interface {
+	Create(user *User) error
+	GetByID(id string) (*User, error)
+	GetByEmail(email string) (*User, error)
+	Update(user *User) error
+	Delete(id string) error
+}
